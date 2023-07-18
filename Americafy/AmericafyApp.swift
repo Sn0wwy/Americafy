a4f0fb1b8282a6f81bba7301303cdce0c1b42a1a
Ignacio Alavés <nachoalasanchez@gmail.com>
2023-07-18 16:19:52 +0200

    Initial Commit

---
+++ Americafy/AmericafyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmericafyApp.swift
+//  Americafy
+//
+//  Created by Nacho Alaves on 18/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AmericafyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Americafy/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Americafy
+//
+//  Created by Nacho Alaves on 18/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var meters = 0.0
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Distance in meters", value: $meters, format: .number)
+                        .keyboardType(.decimalPad)
+                } header: {
+                    Text("Meters")
+                }
+                
+                Section {
+                    Text((meters / 1000), format: .number)
+                } header: {
+                    Text("Kilometers")
+                }
+                
+                Section {
+                    Text((meters * 3.281), format: .number)
+                } header: {
+                    Text("Feet")
+                }
+                
+                Section {
+                    Text((meters * 1.094), format: .number)
+                } header: {
+                    Text("Yards")
+                }
+                
+                Section {
+                    Text((meters / 1609), format: .number)
+                } header: {
+                    Text("Miles")
+                }
+                
+                
+                
+                
+            }
+            .navigationTitle("Americafy")
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
